3fbdca42f9ba4b1d1ba3e9394fb16fbaf1635c1b
Timur Kalabin <truthfire.bro@gmail.com>
2024-06-25 17:03:26 +0300

    Most of english translation strings for dashboard

---
+++ lang/en/dashboard.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'balance' => 'Balance',
+    'today' => 'Today',
+    'on_referrals' => 'On referrals',
+    'today_online' => 'Today online',
+    'completed_tasks' => 'Completed tasks',
+    'earning_lvl' => 'Earning level',
+    'withdraw_title' => 'Withdrawals',
+    'withdraw' => 'Withdraw',
+    'withdraw_rate' => 'At the rate of 100 points = 0.01 USD.',
+    'wallet_num' => 'Wallet number',
+    'amount' => 'Amount (in points)',
+    'day_limit' => 'Daily limit',
+    'points' => 'point|points',
+    'affiliate_title' => 'Affiliate program',
+    'affiliate_rates' => 'Invite new users and get 10-5-2% of their income for life.',
+    'ref_link' => 'Referral link',
+    'ref_num' => 'Number of referrals',
+    'lvl_1' => 'First level',
+    'lvl_2' => 'Second level',
+    'lvl_3' => 'Third level',
+    'today_refs' => 'Today\'s referrals',
+    'ref_list' => 'Referral list',
+    'promo_materials' => 'Promotional materials',
+
+];
